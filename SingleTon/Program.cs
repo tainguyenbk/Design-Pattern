ee480455c2d8202264635972b3a6081d3bff287a
Nguyen Van Tai <tai.nguyenvan@becamex.com>
2022-07-18 23:18:22 +0700

    add Singleton

---
+++ SingleTon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleTon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread1 = new Thread(() => Singleton.GetSingleton().SayHi());
+            var thread2 = new Thread(() => Singleton.GetSingleton().SayHi());
+            var thread3 = new Thread(() => Singleton.GetSingleton().SayHi());
+            var thread4 = new Thread(() => Singleton.GetSingleton().SayHi());
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+        }
+    }
+}
---
+++ SingleTon/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleTon
+{
+    public class Singleton
+    {
+        private int index;
+        private static readonly object lockObject = new object();
+
+        // volatile: goi uniqueInstance voi version moi nhat
+        private static volatile Singleton uniqueInstance;
+        private Singleton(int index)
+        {
+            this.index = index;
+        }
+
+        public static Singleton GetSingleton()
+        {
+            // neu uniqueInstance null thi khoa ham GetSingleton lai
+            // Dung lock de tranh truong hop tao nhieu Instance khi su dung Thread
+            if (uniqueInstance == null)
+            {
+                lock (lockObject)
+                {
+                    if (uniqueInstance == null)
+                    {
+                        var random = new Random();
+                        // Ham ranom gia tri tu 1 den < 4 (1 den 3)
+                        uniqueInstance = new Singleton(random.Next(1, 4));
+                    }                    
+                }
+            }
+            return uniqueInstance;
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hello, I am Tai number " + index);
+        }
+    }
+}
